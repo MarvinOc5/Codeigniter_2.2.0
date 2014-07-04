25f122a7ce30e5e0cff2b13de72b5b0a1192e69e
MarvinOc5 <marvingiffordoclima@yahoo.com>
2014-07-04 14:28:00 +0800

    Update config.php

--- php/application/config/config.php
+++ php/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://codeigniter3-codeigniteroc5.rhcloud.com/';
 
 /*
 |--------------------------------------------------------------------------
