a8e42a3bd8b50bdfb314f6b672bc8a8ac321f844
MarvinOc5 <marvingiffordoclima@yahoo.com>
2014-07-08 14:39:34 +0800

    Update cproducts.php

--- php/application/controllers/cproducts.php
+++ php/application/controllers/cproducts.php
@@ -2,8 +2,22 @@
 
 class CProducts extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		if ('Administrator'!=$this->session->userdata('userlevel')
+				&&'Salesincharges'!=$this->session->userdata('userlevel')
+				&&'Accountant'!=$this->session->userdata('userlevel')
+				&&'Stockclerk'!=$this->session->userdata('userlevel'))
+		{
+			$this->session->unset_userdata('userid');
+			$this->session->unset_userdata('userlevel');
+			header('Location: '.site_url(''));
+		 	$this->session->set_flashdata('error',' You dont have privilage to access that page(You are not ADMIN)!');
+		}
+	}
 	
-	public function products()
+	function products()
 	{
 		$data['base'] = $this->config->item('base_url');
 		$data['title']= 'The Philippine Newlong Corporation';
