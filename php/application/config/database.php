ee21024497c3b2080a93f39e3562d66e8e2e8793
MarvinOc5 <marvingiffordoclima@yahoo.com>
2014-07-08 10:42:49 +0800

    Update database.php

--- php/application/config/database.php
+++ php/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'myusername';
+$db['default']['password'] = 'mypassword';
+$db['default']['database'] = 'mydatabase';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
