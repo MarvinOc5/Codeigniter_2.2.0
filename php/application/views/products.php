79291315aa0b699df65ac9b8abc80aa3097f6179
MarvinOc5 <marvingiffordoclima@yahoo.com>
2014-07-08 12:23:18 +0800

    Create products.php

---
+++ php/application/views/products.php
@@ -0,0 +1,72 @@
+    
+    <div id="templatemo_main">
+		<div class="post_box"> 
+        	<h2>Web Design and Development</h2>
+        	<div class="post_meta">
+            	<ul>
+                	<li>134 comments</li>
+                	<li>28 Jan 2048</li>
+					<li><a href="#">Development</a>, <a href="#">Web Design</a></li>
+				</ul>
+            </div>
+            <div class="col_b float_r">
+                <img src="images/templatemo_image_12.png" alt="Image 12" class="image_frame_300" />
+                <div class="post_text">
+                    <p>Duis sed elit sapien. Aenean tellus magna, volutpat eu accumsan nec, pharetra id lectus. Praesent rhoncus massa eget lacus pulvinar tincidunt. Cras consequat venenatis lacinia. Nam aliquam leo a nunc condimentum egestas. In imperdiet mauris id massa aliquam nisi fermentum.</p>
+                    <a href="blog_post.html" class="more">More</a>
+                </div>                
+			</div>
+            <div class="cleaner"></div>
+        </div>
+        <div class="post_box"> 
+        	<h2>Free Website Templates</h2>
+        	<div class="post_meta">
+            	<ul>
+                	<li>158 comments</li>
+                	<li>23 Jan 2048</li>
+					<li><a href="#">Illustrations</a>, <a href="#">Graphics</a></li>
+				</ul>
+            </div>
+            <div class="col_b float_r">
+                <img src="images/templatemo_image_13.png" alt="" class="image_frame_300" />
+                <div class="post_text">
+                    <p>Maecenas ut sem dui. Ut aliquet, lorem vel consequat varius, ante est malesuada felis, et accumsan lorem eros id lorem. Aenean commodo mi et est aliquet non congue elit aliquam. Nulla ligula augue, pharetra id bibendum id, congue vel urna. Curabitur pulvinar diam ac libero hendrerit.</p>
+                    <a href="blog_post.html" class="more">More</a>
+                </div>                
+			</div>
+            <div class="cleaner"></div>
+        </div>
+        
+        <div class="post_box"> 
+        	<h2>New CSS Tips and Tricks</h2>
+        	<div class="post_meta">
+            	<ul>
+                	<li>180 comments</li>
+                	<li>12 Jan 2048</li>
+					<li><a href="#">Templates</a>, <a href="#">Freebies</a></li>
+				</ul>
+            </div>
+            <div class="col_b float_r">
+                <img src="images/templatemo_image_14.png" alt="" class="image_frame_300" />
+                <div class="post_text">
+                    <p>Aenean sit amet lacus id enim euismod pretium eu sit amet justo. Integer volutpat laoreet eros, a pharetra urna aliquet sit amet. Nullam sollicitudin condimentum semper. Nam pulvinar aliquam est, vel feugiat ante tincidunt eget. Praesent feugiat erat nec ornare suscipit. Validate <a href="http://validator.w3.org/check?uri=referer" rel="nofollow"><strong>XHTML</strong></a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow"><strong>CSS</strong></a>.</p>
+                    <a href="blog_post.html" class="more">More</a>
+                </div>                
+			</div>
+            <div class="cleaner"></div>
+        </div>
+        
+         <div class="pagging">
+            <ul>
+                <li><a href="http://www.templatemo.com" target="_parent">Previous</a></li>
+                <li><a href="http://www.templatemo.com/page/1" target="_parent">1</a></li>
+                <li><a href="http://www.templatemo.com/page/2" target="_parent">2</a></li>
+                <li><a href="http://www.templatemo.com/page/3" target="_parent">3</a></li>
+                <li><a href="http://www.templatemo.com/page/4" target="_parent">4</a></li>
+                <li><a href="http://www.templatemo.com/page/5" target="_parent">5</a></li>
+                <li><a href="http://www.templatemo.com/page/6" target="_parent">6</a></li>
+                <li><a href="http://www.templatemo.com/page/7" target="_parent">Next</a></li>
+            </ul>
+        </div>
+        <div class="cleaner"></div>
+    </div> <!-- end of main -->
