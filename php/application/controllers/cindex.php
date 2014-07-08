d31080a91fbd42302a47e0dd7895eb3e49fbfe09
MarvinOc5 <marvingiffordoclima@yahoo.com>
2014-07-08 10:14:13 +0800

    Update cindex.php

--- php/application/controllers/cindex.php
+++ php/application/controllers/cindex.php
@@ -7,41 +7,9 @@ class CIndex extends CI_Controller
 	{
 		$data['base'] = $this->config->item('base_url');
 		$data['title']= 'The Philippine Newlong Corporation';
-		$this->load->library('googlemaps');
-		$config['center'] = 'auto';
-		$this->googlemaps->initialize($config);
-
-		$data['map'] = $this->googlemaps->create_map();
 		$data['content']= 'home';
 		$this->load->view('template', $data);
 	}
-	
-	public function about()
-	{
-		$data['base'] = $this->config->item('base_url');
-		$data['title']= 'The Philippine Newlong Corporation';
-		$this->load->library('googlemaps');
-		$config['center'] = 'auto';
-		$this->googlemaps->initialize($config);
-
-		$data['map'] = $this->googlemaps->create_map();
-		$data['content']= 'about';
-		$this->load->view('template', $data);
-	}
-	
-	public function faq()
-	{
-		$data['base'] = $this->config->item('base_url');
-		$data['title']= 'The Philippine Newlong Corporation';
-		$this->load->library('googlemaps');
-		$config['center'] = 'auto';
-		$this->googlemaps->initialize($config);
-
-		$data['map'] = $this->googlemaps->create_map();
-		$data['content']= 'faq';
-		$this->load->view('template', $data);
-	}
-
 }
 
 /* End of file cindex.php */
