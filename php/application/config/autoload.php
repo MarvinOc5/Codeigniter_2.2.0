a44086acfd9db310ba5b152c66875f4b2d96d391
MarvinOc5 <marvingiffordoclima@yahoo.com>
2014-07-09 12:52:56 +0800

    Update autoload.php

--- php/application/config/autoload.php
+++ php/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('form_validation', 'session', 'database');
+$autoload['libraries'] = array('form_validation', 'session');
 
 
 /*
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('muser');
+$autoload['model'] = array();
 
 
 /* End of file autoload.php */
