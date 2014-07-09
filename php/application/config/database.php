fe0e546dfbb8a692201d0957501b80ac9e9e09a4
MarvinOc5 <marvingiffordoclima@yahoo.com>
2014-07-09 08:49:50 +0800

    Update database.php

--- php/application/config/database.php
+++ php/application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'https://codeigntier4-oclima.rhcloud.com/phpmyadmin/';
+$db['default']['username'] = 'admin3ZeSR2l';
+$db['default']['password'] = 'JGcXhxgxVWHz';
+$db['default']['database'] = 'codeigniter5';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
